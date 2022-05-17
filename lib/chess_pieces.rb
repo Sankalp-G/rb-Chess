012cf92e1b6f23b54219ea660fc0c4713588b7af
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-05-18 00:25:17 +0530

    create #default_chess_board

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -9,6 +9,29 @@ module ChessPieces
     @symbol.colorize(@color.to_sym)
   end
 
+  def self.default_chess_board
+    [king_row_of_color('black'),
+     pawn_row_of_color('black'),
+     *Array.new(4) { empty_row },
+     pawn_row_of_color('white'),
+     king_row_of_color('white')]
+  end
+
+  # array of classes for the first row of a chess board
+  def self.king_row_of_color(color)
+    [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook].map { |piece| piece.new(color) }
+  end
+
+  # array of a row of pawns
+  def self.pawn_row_of_color(color)
+    Array.new(8) { Pawn.new(color) }
+  end
+
+  # array of unoccupied classes
+  def self.empty_row
+    Array.new(8) { Unoccupied.new }
+  end
+
   ### All chess piece classes
 
   attr_reader :color
