3ab5359e570f3cda04853238b29ab797d33d4f81
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-05-17 23:27:59 +0530

    create individual chess piece classes

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -2,5 +2,62 @@
 
 # module containing chess piece class and common methods
 module ChessPieces
-  
+  # all chess piece classes
+
+  attr_reader :color, :symbol
+
+  # class for empty tiles
+  class Unoccupied
+    def initialize(_color = nil)
+      @symbol = ' '
+    end
+  end
+
+  # pawn chess piece class
+  class Pawn
+    def initialize(color)
+      @color = color
+      @symbol = '♟'
+    end
+  end
+
+  # knight chess piece class
+  class Knight
+    def initialize(color)
+      @color = color
+      @symbol = '♞'
+    end
+  end
+
+  # bishop chess piece class
+  class Bishop
+    def initialize(color)
+      @color = color
+      @symbol = '♝'
+    end
+  end
+
+  # rook chess piece class
+  class Rook
+    def initialize(color)
+      @color = color
+      @symbol = '♜'
+    end
+  end
+
+  # queen chess piece class
+  class Queen
+    def initialize(color)
+      @color = color
+      @symbol = '♛'
+    end
+  end
+
+  # king chess piece class
+  class King
+    def initialize(color)
+      @color = color
+      @symbol = '♚'
+    end
+  end
 end
