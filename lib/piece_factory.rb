e63aa32f3738fa434bdb80cf9b51c12505ec5294
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-06-01 20:48:25 +0530

    create piece factory method

--- lib/piece_factory.rb
+++ lib/piece_factory.rb
@@ -4,6 +4,24 @@ pieces.each { |piece| require_relative "./piece/#{piece}" }
 
 # used to create pieces and arrange board arrays
 module PieceFactory
+  PIECES = {
+    unoccupied: Unoccupied,
+    pawn: Pawn,
+    knight: Knight,
+    bishop: Bishop,
+    rook: Rook,
+    queen: Queen,
+    king: King
+  }.freeze
+
+  # return specified chess piece object
+  def self.create_piece_of_color(piece_name, color)
+    name_symbol = piece_name.downcase.to_sym
+    raise 'invalid piece name' unless PIECES.include?(name_symbol)
+
+    PIECES[name_symbol].new(color)
+  end
+
   # 2d array of pieces representing a normal chess board
   def self.default_chess_board
     [king_row_of_color('black'),
