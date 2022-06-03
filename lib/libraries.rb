5ebca2bf3ab4d4b2939b437c782c976f2341b4f7
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-06-03 23:15:55 +0530

    create libraries file

---
+++ lib/libraries.rb
@@ -0,0 +1,18 @@
+# External
+require 'colorize'
+
+# Internal
+require_relative 'piece'
+require_relative 'board_constants'
+require_relative 'board'
+require_relative 'coord_pair'
+require_relative 'coord_map'
+
+# Pieces
+require_relative './piece/unoccupied'
+require_relative './piece/pawn'
+require_relative './piece/knight'
+require_relative './piece/bishop'
+require_relative './piece/rook'
+require_relative './piece/queen'
+require_relative './piece/king'
