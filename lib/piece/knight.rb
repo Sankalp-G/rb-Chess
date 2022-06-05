703f6bf7432327d4eb90ff569a32aa16379e0370
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-06-05 17:06:47 +0530

    create valid move method

--- lib/piece/knight.rb
+++ lib/piece/knight.rb
@@ -3,4 +3,21 @@ class Knight < Piece
   def symbol
     '♞'
   end
+
+  def get_valid_move_map(start_coord, board)
+    moves = MoveMap.new(start_coord, knight_coords(start_coord))
+    moves.remove_friendly_fire(board)
+  end
+
+  private
+
+  def knight_coords(start_coord)
+    CoordMap.from_2d_array(relative_coord_arr)
+            .all_add(start_coord)
+            .remove_out_of_bounds
+  end
+
+  def relative_coord_arr
+    [[1, 2], [-1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, 1], [-2, -1]]
+  end
 end
