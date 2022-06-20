8ee3897db8757f9cbb10ba1848e41204c095b8b0
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-06-20 15:37:49 +0530

    remove @is_on_enemy

--- lib/move.rb
+++ lib/move.rb
@@ -2,13 +2,11 @@
 class Move
   attr_reader :start_coord, :destination_coord
 
-  def initialize(start_coord, destination_coord, is_on_enemy: false)
+  def initialize(start_coord, destination_coord)
     raise 'coord arguments must be coord pair' unless start_coord.is_a?(CoordPair) && destination_coord.is_a?(CoordPair)
-    raise 'is_on_enemy argument must be a boolean' unless [true, false].include?(is_on_enemy)
 
     @start_coord = start_coord
     @destination_coord = destination_coord
-    @is_on_enemy = is_on_enemy
   end
 
   def in_bounds?
