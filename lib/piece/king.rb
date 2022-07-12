13941b5d9b3c4caffdb7d9c6832398e11f2875d8
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-07-12 18:41:13 +0530

    create valid move getter method

--- lib/piece/king.rb
+++ lib/piece/king.rb
@@ -3,4 +3,21 @@ class King < Piece
   def symbol
     '♚'
   end
+
+  def get_valid_move_map(start_coord, board)
+    moves = MoveMap.new(start_coord, king_coords(start_coord))
+    moves.remove_friendly_fire(board)
+  end
+
+  private
+
+  def king_coords(start_coord)
+    CoordMap.from_2d_array(relative_coord_arr)
+            .all_add(start_coord)
+            .remove_out_of_bounds
+  end
+
+  def relative_coord_arr
+    [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [-1, 0], [1, -1]]
+  end
 end
