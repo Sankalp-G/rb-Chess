00f259ef136869875d86f357ad383a22676b21a7
Sankalp Gawali <sankalp.gawali@gmail.com>
2022-07-12 18:14:14 +0530

    create valid move getter method

--- lib/piece/queen.rb
+++ lib/piece/queen.rb
@@ -3,4 +3,15 @@ class Queen < Piece
   def symbol
     '♛'
   end
+
+  def get_valid_move_map(start_coord, board)
+    dest_coords = valid_dest_coord_map(start_coord, board)
+    MoveMap.new(start_coord, dest_coords)
+  end
+
+  def valid_dest_coord_map(start_coord, board)
+    Path.new(start_coord, board)
+        .upwards.downwards.leftwards.rightwards.up_left.up_right.down_right.down_left
+        .to_coord_map
+  end
 end
--- lib/piece/rook.rb
+++ lib/piece/rook.rb
@@ -3,4 +3,15 @@ class Rook < Piece
   def symbol
     '♜'
   end
+
+  def get_valid_move_map(start_coord, board)
+    dest_coords = valid_dest_coord_map(start_coord, board)
+    MoveMap.new(start_coord, dest_coords)
+  end
+
+  def valid_dest_coord_map(start_coord, board)
+    Path.new(start_coord, board)
+        .upwards.downwards.leftwards.rightwards
+        .to_coord_map
+  end
 end
