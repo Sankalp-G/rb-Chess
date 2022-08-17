644371f517d41cf150b35437996f180faa82ba01
Sankalp-G <sankalp.gawali@gmail.com>
2022-08-17 21:15:18 +0530

    rename @move to @moves_arr

--- lib/move_map.rb
+++ lib/move_map.rb
@@ -1,34 +1,34 @@
 # combination of several moves originating from the same coord
 class MoveMap
-  attr_reader :start_coord, :moves
+  attr_reader :start_coord, :moves_arr
 
   def initialize(start_coord, destination_coord_map)
     raise 'start_coord must be a coord pair' unless start_coord.is_a?(CoordPair)
     raise 'destination map must be a coord map' unless destination_coord_map.is_a?(CoordMap)
 
     @start_coord = start_coord
-    @moves = convert_to_moves(destination_coord_map)
+    @moves_arr = convert_to_moves(destination_coord_map)
   end
 
   def each_move(&block)
-    @moves.each(&block)
+    @moves_arr.each(&block)
   end
 
   def remove_friendly_fire(board)
-    @moves.filter! { |move| !move.targeting_friendly?(board) }
+    @moves_arr.filter! { |move| !move.targeting_friendly?(board) }
     self
   end
 
   def dest_coord_map
-    destination_coords = @moves.map(&:destination_coord)
+    destination_coords = @moves_arr.map(&:destination_coord)
     CoordMap.new(destination_coords)
   end
 
   def ==(other)
-    return false if @moves.length != other.moves.length
+    return false if @moves_arr.length != other.moves_arr.length
 
-    @moves.length.times do |i|
-      return false unless @moves[i] == other.moves[i]
+    @moves_arr.length.times do |i|
+      return false unless @moves_arr[i] == other.moves_arr[i]
     end
     true
   end
