72a87f2766f9ea2d056da6dc2f653e6a1ecfd4f4
Sankalp-G <sankalp.gawali@gmail.com>
2022-10-02 21:42:24 +0530

    refactor variables

--- spec/castling_spec.rb
+++ spec/castling_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../lib/libraries'
 
 describe Castling do
+  let(:castle_board) { Board.new }
+
   describe '#can_castle_left?' do
-    let(:castle_board) { Board.new }
     let(:king_coord) { CoordPair.new(7, 4) }
+    let(:castle_obj) { described_class.new(king_coord, castle_board) }
 
     before do
       # clear path between king and rook
@@ -14,7 +16,6 @@ describe Castling do
 
     context 'when all castling conditions are satisfied' do
       it 'returns true' do
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_left?).to be(true)
       end
     end
@@ -22,28 +23,25 @@ describe Castling do
     context 'when castling conditions are not satisfied' do
       it 'returns false when path between rook and king is blocked' do
         castle_board.place_object_at_coord(Knight.new(:white), CoordPair.new(7, 2))
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_left?).to be(false)
       end
 
       it 'returns false if king is in check' do
         # place rook in front of king
         castle_board.place_object_at_coord(Rook.new(:black), CoordPair.new(6, 4))
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_left?).to be(false)
       end
 
       it 'returns false if king path is in check' do
         castle_board.place_object_at_coord(Rook.new(:black), CoordPair.new(6, 2))
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_left?).to be(false)
       end
     end
   end
 
   describe '#can_castle_right?' do
-    let(:castle_board) { Board.new }
     let(:king_coord) { CoordPair.new(0, 4) }
+    let(:castle_obj) { described_class.new(king_coord, castle_board) }
 
     before do
       # clear path between king and rook
@@ -54,7 +52,6 @@ describe Castling do
 
     context 'when all castling conditions are satisfied' do
       it 'returns true' do
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_right?).to be(true)
       end
     end
@@ -62,20 +59,17 @@ describe Castling do
     context 'when castling conditions are not satisfied' do
       it 'returns false when path between rook and king is blocked' do
         castle_board.place_object_at_coord(Knight.new(:black), CoordPair.new(0, 6))
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_right?).to be(false)
       end
 
       it 'returns false if king is in check' do
         # place rook in front of king
         castle_board.place_object_at_coord(Rook.new(:white), CoordPair.new(1, 4))
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_right?).to be(false)
       end
 
       it 'returns false if king path is in check' do
         castle_board.place_object_at_coord(Rook.new(:white), CoordPair.new(1, 6))
-        castle_obj = described_class.new(king_coord, castle_board)
         expect(castle_obj.can_castle_right?).to be(false)
       end
     end
