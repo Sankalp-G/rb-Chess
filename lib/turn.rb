baa2a94ae4416bb1477f97fd9530b1e5c1730a3e
Sankalp-G <sankalp.gawali@gmail.com>
2022-12-29 19:08:01 +0530

    create Turn class

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+# class handling interactions for a single turn for one player in chess
+class Turn
+  def initialize(board, active_player)
+    @board = board
+    @active_player = active_player
+  end
+
+  def play
+    # display board
+    # get coord from player
+    # select piece on coord
+    # display viable moves
+    # select move from destination coord from user
+    # play move on board
+  end
+end
